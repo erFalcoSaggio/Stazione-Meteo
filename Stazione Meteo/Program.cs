82f0de78802b25532165321aa9c77fdadb21c7c9
12999548 <12999548@INFO1-04>
2025-04-05 08:05:27 +0200

    Aggiungere i file di progetto.

---
+++ Stazione Meteo/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Stazione_Meteo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
